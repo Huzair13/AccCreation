9b60335095762a31d88cc8c26592dabebfc86c1d
Huzair13 <ahamedhuzair13@gmail.com>
2025-05-31 15:48:41 +0530

    Initial commit

--- envs/global/org-setup/import_ous.sh
+++ envs/global/org-setup/import_ous.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-# Set the AWS account ID
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-
-# Set the root OU ID
-ROOT_OU_ID=$(aws organizations list-roots --query 'Roots[0].Id' --output text)
+# Initialize Terraform
+terraform init -reconfigure -backend-config=backend.tfbackend
 
 # Function to get OU ID
 get_ou_id() {
     local ou_name=$1
-    aws organizations list-organizational-units-for-parent --parent-id $ROOT_OU_ID --query "OrganizationalUnits[?Name=='$ou_name'].Id" --output text
+    aws organizations list-organizational-units-for-parent --parent-id $(aws organizations list-roots --query 'Roots[0].Id' --output text) --query "OrganizationalUnits[?Name=='$ou_name'].Id" --output text
 }
 
 # Import OUs
-import_ou() {
-    local ou_name=$1
-    local ou_id=$(get_ou_id $ou_name)
-    if [ -n "$ou_id" ]; then
+import_ous() {
+    local root_id=$(aws organizations list-roots --query 'Roots[0].Id' --output text)
+    aws organizations list-organizational-units-for-parent --parent-id $root_id --query 'OrganizationalUnits[].[Name, Id]' --output text | while read -r ou_name ou_id; do
         echo "Importing $ou_name OU..."
         terraform import "module.org.aws_organizations_organizational_unit.ou[\"$ou_name\"]" $ou_id
-    else
-        echo "Error: Could not find OU with name $ou_name"
-    fi
+    done
 }
 
-# Run terraform init
-terraform init
-
-# Import each OU
-import_ou "Security"
-import_ou "SharedServices"
-import_ou "Sandbox"
-import_ou "TestAccount"
-import_ou "Production"
-import_ou "Dev"
+# Import all OUs
+import_ous
 
 echo "OU import completed."
--- envs/global/org-setup/main.tf
+++ envs/global/org-setup/main.tf
@@ -1,80 +1,80 @@
-module "org" {
-  source = "../../../modules/organizations"
-  ous = {
-    Security       = "ROOT"
-    SharedServices = "ROOT"
-    Sandbox        = "ROOT"
-    TestAccount    = "ROOT"
-    Production     = "ROOT"
-    Dev            = "ROOT"
-  }
-}
+# module "org" {
+#   source = "../../../modules/organizations"
+#   ous = {
+#     # Security       = "ROOT"
+#     # SharedServices = "ROOT"
+#     # Sandbox        = "ROOT"
+#     # TestAccount    = "ROOT"
+#     # Production     = "ROOT"
+#     # Dev            = "ROOT"
+#   }
+# }
 
-module "security_account" {
-  source        = "../../../modules/account"
-  name          = "Security"
-  email         = var.security_email
-  parent_ou_id  = module.org.ou_ids["Security"]
-}
+# module "security_account" {
+#   source        = "../../../modules/account"
+#   name          = "Security"
+#   email         = var.security_email
+#   parent_ou_id  = module.org.ou_ids["Security"]
+# }
 
-module "test_account" {
-  source      = "../../../modules/account"
-  name        = "TestAccount"
-  email       = var.test_email
-  parent_ou_id = module.org.ou_ids["TestAccount"]
-}
+# module "test_account" {
+#   source      = "../../../modules/account"
+#   name        = "TestAccount"
+#   email       = var.test_email
+#   parent_ou_id = module.org.ou_ids["TestAccount"]
+# }
 
-module "shared_account" {
-  source        = "../../../modules/account"
-  name          = "SharedServices"
-  email         = var.shared_email
-  parent_ou_id  = module.org.ou_ids["SharedServices"]
-}
+# module "shared_account" {
+#   source        = "../../../modules/account"
+#   name          = "SharedServices"
+#   email         = var.shared_email
+#   parent_ou_id  = module.org.ou_ids["SharedServices"]
+# }
 
-module "scp_baseline" {
-  source      = "../../../modules/scps"
-  name        = "BaselineDeny"
-  description = "Deny everything except a few services"
-  content     = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Deny",
-      "Action": "*",
-      "Resource": "*",
-      "Condition": {
-        "StringNotEquals": {
-          "aws:RequestedRegion": [
-            "us-east-1"
-          ]
-        }
-      }
-    }
-  ]
-}
-EOF
-  targets = {
-    SecurityOU = module.org.ou_ids["Security"]
-    SharedOU   = module.org.ou_ids["SharedServices"]
-  }
-}
+# module "scp_baseline" {
+#   source      = "../../../modules/scps"
+#   name        = "BaselineDeny"
+#   description = "Deny everything except a few services"
+#   content     = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Deny",
+#       "Action": "*",
+#       "Resource": "*",
+#       "Condition": {
+#         "StringNotEquals": {
+#           "aws:RequestedRegion": [
+#             "us-east-1"
+#           ]
+#         }
+#       }
+#     }
+#   ]
+# }
+# EOF
+#   targets = {
+#     SecurityOU = module.org.ou_ids["Security"]
+#     SharedOU   = module.org.ou_ids["SharedServices"]
+#   }
+# }
 
-module "lambda_enroll_account" {
-  source       = "../../../modules/lambda"
-  function_name = "MyEnrollLambda"
-  source_path  = "../../../lambda/enroll_account/lambda_function.py"
-}
+# module "lambda_enroll_account" {
+#   source       = "../../../modules/lambda"
+#   function_name = "MyEnrollLambda"
+#   source_path  = "../../../lambda/enroll_account/lambda_function.py"
+# }
 
-resource "aws_lambda_invocation" "enroll_shared_account" {
-  depends_on    = [module.shared_account, module.lambda_enroll_account]
-  function_name = module.lambda_enroll_account.function_name
-  input = jsonencode({
-    account_id    = module.shared_account.account_id
-    contact_email = var.shared_email
-    ou_id         = module.org.ou_ids["SharedServices"]
-  })
-}
+# resource "aws_lambda_invocation" "enroll_shared_account" {
+#   depends_on    = [module.shared_account, module.lambda_enroll_account]
+#   function_name = module.lambda_enroll_account.function_name
+#   input = jsonencode({
+#     account_id    = module.shared_account.account_id
+#     contact_email = var.shared_email
+#     ou_id         = module.org.ou_ids["SharedServices"]
+#   })
+# }
 
 
 variable "new_accounts" {
